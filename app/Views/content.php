7a9c55bcbdc394cc2c2626ef9c03fef1c0eeae8b
Lucas Sachs <lucas_sachs@outlook.com>
2024-02-26 18:55:35 -0300

    Desenvolvimento do fluxo de questoes do quiz

--- app/Views/content.php
+++ app/Views/content.php
@@ -1,20 +1,47 @@
 <?php echo view('main/header') ?>
-    <div class="position-absolute top-50 start-50 translate-middle">
-        <div class="text-center mb-3">
-            <h1>FastQuiz</h1>
-            <h6>Question: 1 of 10</h6>
-        </div>
 
-        <?php foreach($questoes as $questao) { ?>
-            <div class="mb-5">
-                <p class="text-dark text-center h5 mb-3"><?php echo $questao['question']; ?></p>
-                <div class="d-flex justify-content-center gap-3">
-                    <?php
-                        foreach($questao['answer'] as $resposta)
-                            echo "<button class='btn btn-primary'>$resposta</button>";
-                    ?>
-                </div>
-            </div>
-        <?php }; ?> <!-- End foreach --> 
+<div id="main" class="position-absolute top-50 start-50 translate-middle">
+    <div class="text-center mb-4">
+        <h1>FastQuiz</h1>
+        <h6 id="subtitle">Question: 1 of 10</h6>
     </div>
+    <?php foreach($questoes as $key => $questao) { ?>
+        <div hidden id="question_<?php echo ++$key ?>" class="mb-5">
+            <p class="text-dark text-center h5 mb-3"><?php echo $questao['question']; ?></p>
+            <div class="d-flex justify-content-center gap-3">
+                <?php
+                    foreach($questao['answer'] as $resposta)
+                        echo "<button class='btn btn-primary resposta'>$resposta</button>";
+                ?>
+            </div>
+        </div>
+    <?php }; ?> <!-- End foreach --> 
+</div>
+
 <?php echo view('main/footer') ?>
+
+<script>
+    var currentQuestion = 1;
+    
+    // Mostrar a primeira questão
+    $(document).ready(function() {
+        $('#question_1').removeAttr('hidden');
+    });
+
+    $('.resposta').on('click', function() {
+        $('#question_' + currentQuestion).attr('hidden', 'hidden');
+        if(currentQuestion == 10) {
+            Swal.fire({
+                title: "Good job!",
+                text: "You finished the quiz!",
+                icon: "success",
+                confirmButtonText: "<i class='bi bi-hand-thumbs-up-fill'></i> Yaay!",
+            });
+
+            return $('#main').hide();
+        }
+        currentQuestion++;
+        $('#subtitle').text('Question: ' + currentQuestion + ' of 10');
+        $('#question_' + currentQuestion).removeAttr('hidden');
+    });
+</script>
